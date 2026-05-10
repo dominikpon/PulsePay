b2216e649886e0fa9657e7b91c5a98e32fbd8b6e
dominikpon <doominikponik@gmail.com>
2026-05-10 19:40:05 +0200

    Initial commit: PulsePay System Foundation

---
+++ src/main/java/com/pulsepay/PulsepayApplication.java
@@ -0,0 +1,13 @@
+package com.pulsepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsepayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulsepay/PulsepayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsepay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsepayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
